b30f7f5a146150864733b5270e1269f117487ed3
shuyuanliu16 <56378976+shuyuanliu16@users.noreply.github.com>
2020-06-01 15:01:20 +0800

    UI v0.3
    test中更新了金钱显示的界面，添加了事件显示的方法。

--- land/Assets/scripts/[liu] ui/pannelManager.cs
+++ land/Assets/scripts/[liu] ui/pannelManager.cs
@@ -1,11 +1,13 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class pannelManager : MonoBehaviour
 {
-
+    public List<string> contents;
     public Text contentText;
     public int currentNumber;
+    public string currentEvent;
 
     public void setContext(string value)
     {
@@ -15,5 +17,10 @@ public class pannelManager : MonoBehaviour
     public void getDiceNumber() {
         currentNumber = Random.Range(1, 6);
         setContext("go " + currentNumber + " step");
-    } 
+    }
+
+    public void getChance() {
+        currentEvent = contents[Random.Range(0, contents.Count)];
+        setContext(currentEvent);
+    }
 }
